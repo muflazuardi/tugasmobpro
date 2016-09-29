b985afc01c44bc60571c4c6849f19a08228ac9cf
Irfan Abdul Hamid <irfanabdulh@live.com>
2016-09-29 21:24:44 +0700

    update login

--- app/src/main/java/muflazuardi/tampilanlogin/login.java
+++ app/src/main/java/muflazuardi/tampilanlogin/login.java
@@ -8,11 +8,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
+    Button btnlogin, btnregister;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnlogin=(Button)findViewById(R.id.button);
+        btnregister=(Button)findViewById(R.id.btnRegis);
+        username=(EditText)findViewById(R.id.editUser);
+        password=(EditText)findViewById(R.id.editPass);
 
 
     }
